3577084cabdbc57baf78c0c3f5c99f77ce1006f8
vamshi9666 <vamshi123nimmakuri123@gmail.com>
2018-03-05 12:30:58 +0530

    finally able to insert script tags into website

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ ourScript.onload = function() {
 
 var ourScript2 = document.createElement('script');
 // TODO: add "script.js" to web_accessible_resources in manifest.json
-ourScript2.src = chrome.extension.getURL('./lib/p5.min.js');
+ourScript2.src = chrome.extension.getURL('./lib/p5.dom.min.js');
 ourScript2.onload = function() {
     this.remove();
 };
@@ -17,7 +17,7 @@ ourScript2.onload = function() {
 
 var ourScript3 = document.createElement('script');
 // TODO: add "script.js" to web_accessible_resources in manifest.json
-ourScript3.src = chrome.extension.getURL('./lib/p5.min.js');
+ourScript3.src = chrome.extension.getURL('./lib/p5.sound.min.js');
 ourScript3.onload = function() {
     this.remove();
 };
@@ -26,8 +26,10 @@ ourScript3.onload = function() {
 
 var actualScript = document.createElement('script');
 // TODO: add "script.js" to web_accessible_resources in manifest.json
-actualScript.src = chrome.extension.getURL('./lib/p5.min.js');
+actualScript.src = chrome.extension.getURL('./main.js');
 actualScript.onload = function() {
     this.remove();
 };
 (document.head || document.documentElement).appendChild(actualScript);
+
+
