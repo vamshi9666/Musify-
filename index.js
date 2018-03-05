c0779ece35f92ca422ded3729041e8f96a2cb809
vamshi9666 <vamshi123nimmakuri123@gmail.com>
2018-03-05 15:56:12 +0530

    somehow able to edit html

---
+++ index.js
@@ -0,0 +1,37 @@
+var ourScript = document.createElement('script');
+// TODO: add "script.js" to web_accessible_resources in manifest.json
+ourScript.src = chrome.extension.getURL('./lib/p5.min.js');
+ourScript.onload = function() {
+    this.remove();
+};
+(document.head || document.documentElement).appendChild(ourScript);
+console.log("inserted p5 .min");
+
+
+
+var ourScript2 = document.createElement('script');
+// TODO: add "script.js" to web_accessible_resources in manifest.json
+ourScript2.src = chrome.extension.getURL('./lib/p5.dom.min.js');
+ourScript2.onload = function() {
+    this.remove();
+};
+(document.head || document.documentElement).appendChild(ourScript2);
+
+var ourScript3 = document.createElement('script');
+// TODO: add "script.js" to web_accessible_resources in manifest.json
+ourScript3.src = chrome.extension.getURL('./lib/p5.sound.min.js');
+ourScript3.onload = function() {
+    this.remove();
+};
+(document.head || document.documentElement).appendChild(ourScript3);
+
+
+var actualScript = document.createElement('script');
+// TODO: add "script.js" to web_accessible_resources in manifest.json
+actualScript.src = chrome.extension.getURL('./main.js');
+actualScript.onload = function() {
+    this.remove();
+};
+(document.head || document.documentElement).appendChild(actualScript);
+
+
--- main.js
+++ main.js
@@ -1,35 +1,4 @@
-var ourScript = document.createElement('script');
-// TODO: add "script.js" to web_accessible_resources in manifest.json
-ourScript.src = chrome.extension.getURL('./lib/p5.min.js');
-ourScript.onload = function() {
-    this.remove();
-};
-(document.head || document.documentElement).appendChild(ourScript);
-
-
-var ourScript2 = document.createElement('script');
-// TODO: add "script.js" to web_accessible_resources in manifest.json
-ourScript2.src = chrome.extension.getURL('./lib/p5.dom.min.js');
-ourScript2.onload = function() {
-    this.remove();
-};
-(document.head || document.documentElement).appendChild(ourScript2);
-
-var ourScript3 = document.createElement('script');
-// TODO: add "script.js" to web_accessible_resources in manifest.json
-ourScript3.src = chrome.extension.getURL('./lib/p5.sound.min.js');
-ourScript3.onload = function() {
-    this.remove();
-};
-(document.head || document.documentElement).appendChild(ourScript3);
-
-
-var actualScript = document.createElement('script');
-// TODO: add "script.js" to web_accessible_resources in manifest.json
-actualScript.src = chrome.extension.getURL('./main.js');
-actualScript.onload = function() {
-    this.remove();
-};
-(document.head || document.documentElement).appendChild(actualScript);
-
-
+function setup() {
+    createCanvas(innerWidth, innerHeight);
+    background(155);
+}
