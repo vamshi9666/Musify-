0a82880c6c25d74f6d7ed8d8a072dde0beea5daa
vamshi9666 <vamshi123nimmakuri123@gmail.com>
2018-03-05 18:37:03 +0530

    blah blah

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
+
 function setup() {
-    createCanvas(innerWidth, innerHeight);
+    createCanvas(inner, height);
     background(155);
+}
+function draw(){
+    ellipse(x, y, 80, 80);
+    x = x + 1;
+    y =y +1;
+    if (x < innerWidth){
+        x -1;
+    }
+    if ( y < innerHeight){
+        y -1;
+    }
 }
