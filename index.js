365dfb861c7f1eb8e12e35f0ca1727aa6f1b5e13
vamshi9666 <vamshi123nimmakuri123@gmail.com>
2018-03-06 11:19:51 +0530

    succesfully able to use dom and sound libararies

--- index.js
+++ index.js
@@ -1,37 +1,8 @@
-var ourScript = document.createElement('script');
-// TODO: add "script.js" to web_accessible_resources in manifest.json
-ourScript.src = chrome.extension.getURL('./lib/p5.min.js');
-ourScript.onload = function() {
-    this.remove();
-};
-(document.head || document.documentElement).appendChild(ourScript);
-console.log("inserted p5 .min");
-
-
-
-var ourScript2 = document.createElement('script');
-// TODO: add "script.js" to web_accessible_resources in manifest.json
-ourScript2.src = chrome.extension.getURL('./lib/p5.dom.min.js');
-ourScript2.onload = function() {
-    this.remove();
-};
-(document.head || document.documentElement).appendChild(ourScript2);
-
-var ourScript3 = document.createElement('script');
-// TODO: add "script.js" to web_accessible_resources in manifest.json
-ourScript3.src = chrome.extension.getURL('./lib/p5.sound.min.js');
-ourScript3.onload = function() {
-    this.remove();
-};
-(document.head || document.documentElement).appendChild(ourScript3);
-
-
-var actualScript = document.createElement('script');
-// TODO: add "script.js" to web_accessible_resources in manifest.json
-actualScript.src = chrome.extension.getURL('./main.js');
-actualScript.onload = function() {
-    this.remove();
-};
-(document.head || document.documentElement).appendChild(actualScript);
-
-
+console.log("started ");
+function setup(){
+    let c = createCanvas(500, 500);
+    c.position(0, 0);
+}
+function draw(){
+    background(0);
+}
