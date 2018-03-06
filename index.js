ba9f2b2ab45c0f56a5c20ca30dbcbfe898bcb041
vamshi9666 <vamshi123nimmakuri123@gmail.com>
2018-03-06 20:23:46 +0530

    days ending

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-console.log("started ");
+var mic;
+var 
+
+function windowResized(){
+    resizeCanvas();
+}
 function setup(){
-    let c = createCanvas(500, 500);
+    let c = createCanvas(windowWidth, windowHeight);
     c.position(0, 0);
+    c.style('z-index','-1');
+    console.log("started ");
+    mic = new p5.AudioIn();
+    mic.start()
 }
 function draw(){
     background(0);
+    var vol = mic.getLevel();
+    ellipse(width/2,height/2,vol*800)    ;
 }
