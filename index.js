5da138ad7c7f5ae4093bdf81828afbf6f614182f
vamshi9666 <vamshi123nimmakuri123@gmail.com>
2018-03-10 22:02:38 +0530

    support css for firefox

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ var mic;
 
 
 function windowResized() {
-    c = resizeCanvas();
+    // c = resizeCanvas();
 }
 function setup() {
-    let c = createCanvas(windowWidth, windowHeight);
-    c.position(0, 0);
-    c.style('z-index', '-1');
-    console.log("started ");
-    mic = new p5.AudioIn();
-    mic.start()
+    // let c = createCanvas(windowWidth, windowHeight);
+    // c.position(0, 0);
+    // c.style('z-index', '-1');
+    // console.log("started ");
+    // mic = new p5.AudioIn();
+    // mic.start()
 }
 function draw() {
-    background(0);
-    var vol = mic.getLevel();
-    ellipse(width / 2, height / 2, vol * 800);
+    // background(0);
+    // var vol = mic.getLevel();
+    // ellipse(width / 2, height / 2, vol * 800);
 
 }
